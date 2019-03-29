5297ffa571bc5d7fe0d973c37ddeec7221b9c63e
zwang695 <zwang695@hhs.win.fuhsd.org>
2019-03-29 13:05:03 -0700

    adsf

--- NetflixPrize/src/Movie.java
+++ NetflixPrize/src/Movie.java
@@ -19,8 +19,35 @@ public class Movie {
 	}
 	
 	public String toString() {
+		StringBuffer s = new StringBuffer();
+		s.append(id);
+		s.append(",");
+		if(title.indexOf("\"") == -1) {
+			s.append(title);
+			if(year != -1) s.append("(" + year + ")");
+		}
+		else {
+			s.append("\"");
+			s.append(title.substring(1, title.length() - 1));
+			if(year != -1) s.append("(" + year + ")");
+			s.append("\"");
+		}
+		s.append(",");
+		for(String f : genres) {
+			s.append(f);
+			s.append("|");
+		}
+		s.append(",");
+		s.append(imdb);
+		s.append(",");
+		s.append(tmdb);
 		
-		return null;
+		return s.toString();
+	}
+	
+	public void setLinks(String imdb, String tmdb) {
+		this.imdb = imdb;
+		this.tmdb = tmdb;
 	}
 
 }
--- NetflixPrize/src/Tester.java
+++ NetflixPrize/src/Tester.java
@@ -8,18 +8,46 @@ public class Tester {
 		ArrayList<Movie> movies = new ArrayList<Movie>();
 		try {
 			String moviesFile = "data" + FileIO.fileSep + "movies.csv";
+			String linksFile = "data" + FileIO.fileSep + "links.csv";
 			ArrayList<String> movieStrs = FileIO.readFile(moviesFile);
+			ArrayList<String> linkStrs = FileIO.readFile(linksFile);
 			int count = 0;
-			for(String s : movieStrs) {
+			for(int i = 0; i < movieStrs.size(); i++) {
 				if(count == 0) {
 					count++;
 					continue;
 				}
-				Movie m = translator.translateMovie(s);
+				Movie m = translator.translateMovie(movieStrs.get(i));
+				translator.translateLinks(m, linkStrs.get(i));
 				movies.add(m);
 			}
 		} catch(IOException e) {
 			e.printStackTrace();
+		}
+		
+		for(Movie m : movies) {
+			System.out.println(m);
+		}
+		
+		ArrayList<Rating> ratings = new ArrayList<Rating>();
+		try {
+			String ratingsFile = "data" + FileIO.fileSep + "ratings.csv";
+			ArrayList<String> ratingStrs = FileIO.readFile(ratingsFile);
+			int count = 0;
+			for(String s : ratingStrs) {
+				if(count == 0) {
+					count++;
+					continue;
+				}
+				Rating r = translator.translateRating(s);
+				ratings.add(r);
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		for(Rating r : ratings) {
+			System.out.println(r);
 		}
 	}
 	
